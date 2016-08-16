fc99eb7558af9a10799f7aeb43e401c8219bd914
breathelove <yudor@163.com>
2016-08-16 10:43:31 +0800

    这是第二个测试版本，直接复制进去Android项目，提交

---
+++ PB_Project_CartoonReader/src/com/comic/activity/sys/LoadingActivity.java
@@ -0,0 +1,10 @@
+package com.comic.activity.sys;
+import android.app.Activity;
+import android.os.Bundle;
+import com.comic.R;
+public class LoadingActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_loading);       
+    }   
+}
